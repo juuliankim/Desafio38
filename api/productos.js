64456fd4ddea2d32be5cb245841286616b556aa8
juuliankim <71047720+juuliankim@users.noreply.github.com>
2021-10-05 18:11:09 -0300

    Add files via upload

---
+++ api/productos.js
@@ -0,0 +1,48 @@
+const Producto = require('../models/productos')
+
+class Productos {
+    constructor() { }
+
+    async listar() {
+        try {
+            return Producto.find({})
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async listarPorId(id) {
+        try {
+            return Producto.findById({ _id: id })
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async guardar(nuevoProducto) {
+        try {
+            console.log(nuevoProducto)
+            return Producto.create(nuevoProducto)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async actualizar(idProducto, nuevoProducto) {
+        try {
+            return Producto.findByIdAndUpdate(idProducto, nuevoProducto)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async borrar(idProducto) {
+        try {
+            return Producto.findByIdAndDelete(idProducto)
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+module.exports = new Productos()
